7a8cf741c005a1e8b7d74612bb4e77420a5917e8
liuyuan <1922381022@qq.com>
2023-06-18 22:34:44 +0800

    test: app bar的grid布局

--- src/layout/components/AppBar.vue
+++ src/layout/components/AppBar.vue
@@ -5,17 +5,27 @@
 </script>
 
 <script lang='ts' setup>
-    // import SearchBar from '@/layout/components/SearchBar.vue';
-    // import SwitchBar from '@/layout/components/SwitchBar.vue';
-    // import PersonalBar from '@/layout/components/PersonalBar.vue';
+
 </script>
 
 <template>
-    <div id='app-bar'></div>
+    <div id='app-bar'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <img src='@/assets/images/avatar.jpg' alt='' class='avatar' />
+    </div>
 </template>
 
 <style lang='scss' scoped>
     #app-bar {
-        background-color: pink;
+        display: grid;
+        grid-template-columns: 4fr 4fr 2fr 40px;
+        grid-template-rows: minmax(40px, auto);
+        grid-column-gap: 10px;
+
+        .avatar {
+            border-radius: 10px;
+        }
     }
 </style>
