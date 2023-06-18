12a36a49ef13233801994c2f9bcea2d30d50c2d7
liuyuan <1922381022@qq.com>
2023-06-18 16:01:13 +0800

    feat: Naive-ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import electron from 'vite-plugin-electron';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 import path from 'path';
 
-// https://vitejs.dev/config/
-export default ({ mode }) => {
+
+// vite配置官网 https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
     // 加载环境变量
     const env = loadEnv(mode, process.cwd());
 
-    return defineConfig({
+    return {
         plugins: [
             vue(),
             electron([
@@ -23,7 +27,23 @@ export default ({ mode }) => {
                         options.reload();
                     }
                 }
-            ])
+            ]),
+            AutoImport({
+                imports: [
+                    'vue',
+                    {
+                        'naive-ui': [
+                            'useDialog',
+                            'useMessage',
+                            'useNotification',
+                            'useLoadingBar'
+                        ]
+                    }
+                ]
+            }),
+            Components({
+                resolvers: [NaiveUiResolver()]
+            })
         ],
         // 相对路径的代称 @
         base: './',
@@ -41,9 +61,10 @@ export default ({ mode }) => {
                     changeOrigin: true, // 允许跨域
                     secure: false, // 如果是不是https接口，可以不配置这个参数
                     ws: true, //代理 web socked
-                    rewrite: (path) => path.replace(/^\/api/, '')
+                    rewrite: (path) => path.replace(/^\/api/,
+                        '')
                 }
             }
         }
-    });
-};
+    };
+});
